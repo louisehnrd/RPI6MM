f937a8f07b4611a9974270276ee55158890ca9ec
louisehnrd <134003016+louisehnrd@users.noreply.github.com>
2023-07-25 08:48:51 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Chemin complet du fichier Python
+python_script="/home/<name_camera>/user_space/periodic_picture.py"
+
+# Activation de l'environnement virtuel Python si nécessaire
+# source /chemin/vers/mon/environnement/virtuel/bin/activate
+
+# Lancement du script Python
+python3 $python_script
